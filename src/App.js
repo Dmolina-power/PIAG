6fd4dab4338024058113f81167caea72415a904e
daniel molina <bigdpower@icloud.com>
2020-07-26 12:49:07 -0700

    testing components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ButtonAppBar from "./Components/NavBar";
+
+// STYLE
+import "./App.css";
+
+// PAGES
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      
-        
-        <p>
-          piag museum
-        </p>
-       
-      
+      <Router>
+        <ButtonAppBar />
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+      </Router>
     </div>
   );
 }
